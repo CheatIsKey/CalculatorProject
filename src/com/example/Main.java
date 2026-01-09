b600754b8111bf16e01bdbb45ce85609682c0939
재민 <silvull2t@naver.com>
2026-01-09 12:56:52 +0900

    Step 2. 클래스를 적용해 기본적인 연산을 수행할 수 있는 계산기 만들기

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,4 +1,52 @@
 package com.example;
 
+import com.example.calculator.Calculator;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        String input = "";
+
+        //TODO: 최초 1회는 실행되고 그 이후에 반복할지 여부를 판단하기 위해, do-while() 반복문 사용하기
+        do {
+            int result = 0;
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int a = scan.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = scan.next().charAt(0);
+
+            if (!(operator == '+' || operator == '-' || operator == '*' || operator == '/')) {
+                System.out.println("잘못된 연산자가 입력되었습니다.");
+                return;
+            }
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int b = scan.nextInt();
+
+//            TODO: 스코프 범위로 인해 객체가 계속 생성되면서 덮어쓰기 된다는 점 조심하기
+//            calculator = new Calculator(a, b, operator);
+
+            try {
+                result = calculator.calculate(a, b,  operator);
+                System.out.println("결과: " + result);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료, delete 입력 시 처음 결과 삭제)");
+            input = scan.next();
+            if (input.equalsIgnoreCase("delete")) {
+                calculator.removeResult();
+            }
+        } while (!input.equalsIgnoreCase("exit"));
+
+        System.out.println(calculator.getList());
+        System.out.println("계산기를 종료합니다.");
+    }
 }
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,40 +1,63 @@
 package com.example.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+//    TODO: 멤버 변수/필드가 꼭 필요한 게 아닌지 먼저 생각해보기
+//    int a;
+//    int b;
+//    char operator;
+//
+//    public Calculator(int a, int b, char operator) {
+//        this.a = a;
+//        this.b = b;
+//        this.operator = operator;
+//    }
+
+//    TODO:
+//      List<Integer> list = new ArrayList<>();
+//      컬렉션 또한 앞에 접근 제어자를 붙일 수 있다!
+    public static List<Integer> list = new ArrayList<>();
+
+    public void setList(List<Integer> list) {
+        Calculator.list = list;
+    }
+
+    public List<Integer> getList() {
+        return list;
+    }
+
+    public void removeResult() {
+        list.remove(0);
+    }
+
+    public int calculate(int a, int b, char operator) {
+        if (operator == '/' && b == 0) {
+            throw new IllegalArgumentException("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+        }
 
         int result = 0;
 
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int a = scan.nextInt();
-
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        String operator = scan.next();
-
-
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        int b = scan.nextInt();
-
         switch (operator) {
-            case "+":
+            case '+':
                 result = a + b;
                 break;
-            case "-":
+            case '-':
                 result = a - b;
                 break;
-            case "*":
+            case '*':
                 result = a * b;
                 break;
-            case "/":
+            case '/':
                 result = a / b;
                 break;
             default:
-                System.out.println("잘못된 연산자가 입력되었습니다.");
+                throw new IllegalArgumentException("잘못된 연산자가 입력되었습니다.");
         }
 
-        System.out.println("결과: " + result);
+        list.add(result);
+        return result;
     }
 }
